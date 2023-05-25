4f6bca3ab1c688940b089862f60bf86048b27542
Crpearce <Crpearce892@gmail.com>
2023-05-25 09:48:20 -0600

    add initial state to test checkboxes

--- src/components/form/form.component.jsx
+++ src/components/form/form.component.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useEffect } from 'react'
 
 import './form.styles.css'
 
@@ -8,23 +9,45 @@ const Form = () => {
   const [newTask, setNewTask] = useState('')
   const [allTasks, setAllTasks] = useState([])
 
+  useEffect(() => {
+    const initialTasks = [
+      {
+        id: 0,
+        date: '2023-05-24',
+        desc: 'task2',
+        completed: false,
+      },
+      {
+        id: 1,
+        date: '2023-06-24',
+        desc: 'task1',
+        completed: true
+      }
+    ]
+    setAllTasks(initialTasks)
+  }, [])
+
   const handleSubmit = (e) => {
     e.preventDefault()
     const newTask = {
       id: Date.now(),
       date: taskDate,
       desc: taskDesc,
+      completed: false,
     }
     setNewTask(newTask)
     setAllTasks([newTask, ...allTasks])
     setTaskDate('')
     setTaskDesc('')
+    console.log(newTask)
   }
 
   const removeTask = (id) => {
     const newArray = allTasks.filter((task) => task.id !== id)
     setAllTasks(newArray)
   }
+
+  console.log(allTasks)
 
   return (
     <div className='form-container'>
@@ -53,18 +76,19 @@ const Form = () => {
         {allTasks.map((task) => {
           return (
             <li key={task.id}>
-                    <label>
-        {' '}
-        Completed:
-        <input
-          type='checkbox'
-          onClick={() => removeTask(task.id)}
-          // value={taskDesc}
-          // placeholder='Add task'
-          // onChange={(e) => setTaskDesc(e.target.value)}
-          // required
-        />
-      </label>
+              <label>
+                {' '}
+                Completed:
+                <input
+                  type='checkbox'
+                  defaultChecked={task.completed}
+                  // onClick={() => removeTask(task.id)}
+                  // value={taskDesc}
+                  // placeholder='Add task'
+                  // onChange={(e) => setTaskDesc(e.target.value)}
+                  // required
+                />
+              </label>
               <h3>{task.date}</h3>
               <p>{task.desc}</p>
               <button onClick={() => removeTask(task.id)}>X</button>
